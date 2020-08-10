b9a00c50f28f6f2da36d4015d587bd7937610a84
浔阳陌客 <61930212+tengfei-xy@users.noreply.github.com>
2020-08-10 18:44:47 +0800

    20200810

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-var data="233"
-var wsdata="ws://127.0.0.1:80/echo"
-var callback = function(e){console.log(e,e.data)}
-websocket(wsdata, data, callback)
+// var data="233"
+// var wsdata="ws://127.0.0.1:80/echo"
+// var callback = function(e){console.log(e,e.data)}
+// websocket(wsdata, data, callback)
+
+var vm = new Vue({
+    el: '#add',
+    data: {
+        server_show: false,
+        project_show: false
+    },
+    methods: {
+        server_display: function() {
+            this.server_show = true
+            this.project_show = false
+        },
+        project_display: function() {
+            this.server_show = false
+            this.project_show = true
+        }
+    }
+
+});
